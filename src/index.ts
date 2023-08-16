d9aad5d40b96b915748826b21ca3377c43672952
naveenrs123 <naveen.sivasankar1234@gmail.com>
2023-08-15 21:50:16 -0700

    Enable url redirection

--- src/index.ts
+++ src/index.ts
@@ -26,23 +26,41 @@ app.post('/', (req: Request<unknown, ShortenedUrlRes, ShortenedUrlReq>, res: Res
     return;
   }
 
-  let keyCollisionRetries = tryRetrieveValidShortUrl(url, res);
-
-  if (keyCollisionRetries < 3) {
-    res.status(500).send("ERROR: Unable to save. Exceeded maximum number of key collision retries.")
+  const hasInvalidHttpPrefix = !(url.startsWith('http://') || url.startsWith('https://'));
+  if (hasInvalidHttpPrefix) {
+    res.status(400).send('URLs must start with "http://" or "https://" for successful redirection.');
   }
+
+  const keyCollisionRetries = tryRetrieveValidShortUrl(url, res);
+  if (keyCollisionRetries === 3) {
+    res.status(500).send('ERROR: Unable to save. Exceeded maximum number of key collision retries.');
+  }
+});
+
+app.get('/:shortUrl', (req: Request, res: Response) => {
+  const key = req.params['shortUrl'];
+  const errorMessage = 'No long URL matches this short URL.';
+
+  console.log(key);
+
+  if (key == null || key.trim().length === 0 || !(key in urlStore)) {
+    res.setHeader('Content-Length', errorMessage.length).status(404).send('No long URL matches this short URL.');
+    return;
+  }
+
+  res.redirect(urlStore[key]);
 });
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
 
-function tryRetrieveValidShortUrl(url: string, res: express.Response<ShortenedUrlRes, Record<string, any>>) {
+function tryRetrieveValidShortUrl(url: string, res: express.Response<ShortenedUrlRes, Record<string, unknown>>) {
   let keyCollisionRetries = 0;
 
   while (keyCollisionRetries < 3) {
-    let urlHash = crypto.createHash('shake128', { outputLength: 5 + keyCollisionRetries });
-    let key = urlHash.update(url).digest('hex');
+    const urlHash = crypto.createHash('shake128', { outputLength: 5 + keyCollisionRetries });
+    const key = urlHash.update(url).digest('hex');
 
     if (key in urlStore) {
       if (urlStore[key] !== url) {
@@ -56,7 +74,7 @@ function tryRetrieveValidShortUrl(url: string, res: express.Response<ShortenedUr
     res.send({
       key: key,
       longUrl: urlStore[key],
-      shortUrl: `http://localhost:8000/${key}`,
+      shortUrl: `http://localhost:${port}/${key}`,
     });
 
     break;
@@ -64,4 +82,3 @@ function tryRetrieveValidShortUrl(url: string, res: express.Response<ShortenedUr
 
   return keyCollisionRetries;
 }
-
